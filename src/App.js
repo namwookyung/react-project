bb0880611d2e8c47b1270c657678fe18b3ba5629
namwookyung <namwookyung.dev@gmail.com>
2022-09-08 15:21:34 +0900

    react-project_22.09.08

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import EventPractice from "./EventPractice";
+import { Component } from "react";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
 
-const App = () => {
-  return <EventPractice />;
-};
+// 랜덤 색상을 생성한다.
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
+}
 
 export default App;
